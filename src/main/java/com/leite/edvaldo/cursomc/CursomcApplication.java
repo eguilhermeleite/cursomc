45db005979d132ef9cea975f1a02c254ac927ec5
Edvaldo Leite <eguilhermeleite@gmail.com>
2020-11-26 00:00:22 -0300

    Pedido,Pagamento e EstadoPagamento

--- src/main/java/com/leite/edvaldo/cursomc/CursomcApplication.java
+++ src/main/java/com/leite/edvaldo/cursomc/CursomcApplication.java
@@ -1,6 +1,6 @@
 package com.leite.edvaldo.cursomc;
 
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +13,20 @@ import com.leite.edvaldo.cursomc.domain.Cidade;
 import com.leite.edvaldo.cursomc.domain.Cliente;
 import com.leite.edvaldo.cursomc.domain.Endereco;
 import com.leite.edvaldo.cursomc.domain.Estado;
+import com.leite.edvaldo.cursomc.domain.Pagamento;
+import com.leite.edvaldo.cursomc.domain.PagamentoComBoleto;
+import com.leite.edvaldo.cursomc.domain.PagamentoComCartao;
+import com.leite.edvaldo.cursomc.domain.Pedido;
 import com.leite.edvaldo.cursomc.domain.Produto;
+import com.leite.edvaldo.cursomc.domain.enums.EstadoPagamento;
 import com.leite.edvaldo.cursomc.domain.enums.TipoCliente;
 import com.leite.edvaldo.cursomc.repositories.CategoriaRepository;
 import com.leite.edvaldo.cursomc.repositories.CidadeRepository;
 import com.leite.edvaldo.cursomc.repositories.ClienteRepository;
 import com.leite.edvaldo.cursomc.repositories.EnderecoRepository;
 import com.leite.edvaldo.cursomc.repositories.EstadoRepository;
+import com.leite.edvaldo.cursomc.repositories.PagamentoRepository;
+import com.leite.edvaldo.cursomc.repositories.PedidoRepository;
 import com.leite.edvaldo.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -38,6 +45,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -68,9 +79,23 @@ public class CursomcApplication implements CommandLineRunner {
 
 		Endereco e1 = new Endereco(null, "Rua Manuel Dias de Oliveira", "59", "Torre A Apto. 172", "Butantã",
 				"05528-010", cli1, c2);
-		
+
 		Endereco e2 = new Endereco(null, "Rua Antunes Ferreira", "102", "-", "Siopi", "03252-025", cli1, c4);
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("25/05/2020 14:23"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("28/06/2020 10:41"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2020 00:00"),
+				null);
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+	
 
 		// Objetos
 		est1.getCidades().addAll(Arrays.asList(c1));
@@ -89,7 +114,9 @@ public class CursomcApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3, c4));
 		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		pedidoRepository.saveAll(Arrays.asList(ped1));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 
 	}
 
---
+++ src/main/java/com/leite/edvaldo/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.leite.edvaldo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leite.edvaldo.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
