4bfc49682112756cf3cbb192336eaecfc46b993c
Edvaldo Leite <eguilhermeleite@gmail.com>
2020-12-08 21:28:54 -0300

    Listar somente as categorias sem os produtos com DTO

--- src/main/java/com/leite/edvaldo/cursomc/services/CategoriaService.java
+++ src/main/java/com/leite/edvaldo/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.leite.edvaldo.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +43,10 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new com.leite.edvaldo.cursomc.services.exceptions.DataIntegrityViolationException("Impossível excluir essa categoria pois existe produto(s) associado(s)");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 
 }
