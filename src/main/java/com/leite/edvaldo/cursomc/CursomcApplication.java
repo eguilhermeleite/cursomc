74a35be0d1077a2a8ad07b63ef511a366b232d90
Edvaldo Leite <eguilhermeleite@gmail.com>
2021-01-20 00:00:15 -0300

    Busca de Produtos por nome e Categorias

--- src/main/java/com/leite/edvaldo/cursomc/CursomcApplication.java
+++ src/main/java/com/leite/edvaldo/cursomc/CursomcApplication.java
@@ -74,6 +74,15 @@ public class CursomcApplication implements CommandLineRunner {
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
+		Produto p4 = new Produto(null, "Mesa de escritório", 300.00);
+		Produto p5 = new Produto(null, "Parafusadeira", 750.00);
+		Produto p6 = new Produto(null, "Colcha", 200.00);
+		Produto p7 = new Produto(null, "Abajour", 500.00);
+		Produto p8 = new Produto(null, "Tv True Color", 1400.00);
+		Produto p9 = new Produto(null, "Toalha", 29.90);
+		Produto p10 = new Produto(null, "Pneu aro 20", 350.00);
+		Produto p11 = new Produto(null, "Regador", 40.00);
+		Produto p12 = new Produto(null, "Shampoo", 30.00);
 
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
@@ -92,14 +101,13 @@ public class CursomcApplication implements CommandLineRunner {
 		Cliente cli2 = new Cliente(null, "Luciene da Silva Leite", "lsilvaleite14@gmail.com", "27158787885",
 				TipoCliente.PESSOA_FISICA);
 		cli2.getTelefones().addAll(Arrays.asList("(11)96125-7615", "(11)96932-4269"));
-	
+
 		Endereco e1 = new Endereco(null, "Rua Manuel Dias de Oliveira", "59", "Torre A Apto. 172", "Butantã",
 				"05528-010", cli1, c2);
 		Endereco e2 = new Endereco(null, "Rua Antunes Ferreira", "102", "-", "Siopi", "03252-025", cli1, c4);
-		
+
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		cli2.getEnderecos().addAll(Arrays.asList(e1, e2));
-		
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("25/05/2020 14:23"), cli1, e1);
@@ -123,11 +131,27 @@ public class CursomcApplication implements CommandLineRunner {
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
+		cat2.getProdutos().addAll(Arrays.asList(p2,p4,p7));
+		cat3.getProdutos().addAll(Arrays.asList(p5));
+		cat4.getProdutos().addAll(Arrays.asList(p6,p9));
+		cat5.getProdutos().addAll(Arrays.asList(p11));
+		cat6.getProdutos().addAll(Arrays.asList(p12));
+		cat7.getProdutos().addAll(Arrays.asList(p10));
+		cat8.getProdutos().addAll(Arrays.asList(p8));
+		
 
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p4.getCategorias().addAll(Arrays.asList(cat1));
+		p5.getCategorias().addAll(Arrays.asList(cat3));
+		p6.getCategorias().addAll(Arrays.asList(cat4));
+		p7.getCategorias().addAll(Arrays.asList(cat2));
+		p8.getCategorias().addAll(Arrays.asList(cat8));
+		p9.getCategorias().addAll(Arrays.asList(cat4));
+		p10.getCategorias().addAll(Arrays.asList(cat7));
+		p11.getCategorias().addAll(Arrays.asList(cat5));
+		p12.getCategorias().addAll(Arrays.asList(cat6));
 
 		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
 		ped2.getItens().addAll(Arrays.asList(ip3));
@@ -138,7 +162,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 		// repositorios
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8));
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3,p4,p5,p6,p7,p8,p9,p10,p11,p12));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3, c4));
 		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
