38712bc03078ac9356b68f32acf2915584c9bfe1
Edvaldo Leite <eguilhermeleite@gmail.com>
2021-01-23 00:24:10 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/leite/edvaldo/cursomc/config/DevConfig.java
@@ -0,0 +1,34 @@
+package com.leite.edvaldo.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.leite.edvaldo.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String str;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+
+		if(!"create".equals(str)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}
